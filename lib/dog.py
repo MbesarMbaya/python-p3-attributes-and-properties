e13706b8659df1819650344c0b764ac7d1815891
Roy Mbaya <roymbaya08@gmail.com>
2023-05-19 08:27:26 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +11,50 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        self._name = None
+        self._breed = None
+
+        if name is not None:
+            self.name = name
+        if breed is not None:
+            self.breed = breed
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            raise ValueError("Breed must be in the list of approved breeds.")
+
+
+# Example usage
+my_dog = Dog(name="Max", breed="Beagle")
+
+print(my_dog.name)  # Output: Max
+print(my_dog.breed)  # Output: Beagle
+
+try:
+    my_dog.name = "This is a very long name for a dog."  # Invalid name
+except ValueError as e:
+    print(str(e))  # Output: Name must be a string between 1 and 25 characters.
+
+try:
+    my_dog.breed = "Labrador"  # Invalid breed
+except ValueError as e:
+    print(str(e))  # Output: Breed must be in the list of approved breeds.
--- lib/person.py
+++ lib/person.py
@@ -4,16 +4,48 @@ APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
+    # ...
 ]
 
 class Person:
-    pass
+    def __init__(self, name: str, job: str):
+        self._name = name
+        self._job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            raise ValueError("Name must be a non-empty string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            raise ValueError("Job must be in list of approved jobs.")
+
+# Example usage
+person = Person(name="Guido", job="Sales")
+
+print(person.name)  # Output: Guido
+print(person.job)  # Output: Sales
+
+try:
+    person.name = "This is a very long name for a person."  # Invalid name
+except ValueError as e:
+    print(str(e))  # Output: Name must be a non-empty string between 1 and 25 characters.
+
+try:
+    person.job = "Software Developer"  # Invalid job
+except ValueError as e:
+    print(str(e))  # Output: Job must be in list of approved jobs.
